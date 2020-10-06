26aeba7caa63427ac708728a45229e1b19f097cf
xcash-rocks <dev@xcash.rocks>
2020-10-06 05:23:58 +0200

    Changed dashcards: rearrangement, icons, text; Table BlockHeight centered;

--- src/app/tables/delegates/delegates.component.spec.ts
+++ src/app/tables/delegates/delegates.component.spec.ts
@@ -37,45 +37,45 @@ describe('delegatesComponent', () => {
   // check that all html components are created
   it('should create', () => expect(component).toBeTruthy());
 
-  it('should create dash card one', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard1')).toBeTruthy());
-  it('should set dash card ones title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard1').dashData.title).toBe('TOTAL VOTE COUNT'));
-  it('should set dash card ones property to a number', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard1').dashData.text).toBe(0));
+  it('should create dash card five', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard1')).toBeTruthy());
+  it('should set dash card fives title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard1').dashData.title).toBe('ONLINE STATUS'));
+  it('should set dash card fives property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard1').dashData.text).toBe(''));
 
   it('should create dash card two', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard2')).toBeTruthy());
-  it('should set dash card twos title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard2').dashData.title).toBe('CURRENT DELEGATE RANK'));
+  it('should set dash card twos title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard2').dashData.title).toBe('DELEGATE RANK'));
   it('should set dash card twos property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard2').dashData.text).toBe(0));
 
-  it('should create dash card three', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard3')).toBeTruthy());
-  it('should set dash card threes title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard3').dashData.title).toBe('BLOCK VERIFIER TOTAL ROUNDS'));
-  it('should set dash card threes property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard3').dashData.text).toBe(0));
+  it('should create dash card eight', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard3')).toBeTruthy());
+  it('should set dash card eights title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard3').dashData.title).toBe('BLOCKS FOUND'));
+  it('should set dash card eights property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard3').dashData.text).toBe(0));
 
-  it('should create dash card four', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard4')).toBeTruthy());
-  it('should set dash card fours title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard4').dashData.title).toBe('BLOCK VERIFIERS ONLINE TOTAL ROUNDS'));
-  it('should set dash card fours property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard4').dashData.text).toBe(0));
+  it('should create dash card six', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard4')).toBeTruthy());
+  it('should set dash card sixs title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard4').dashData.title).toBe('ONLINE PERCENTAGE'));
+  it('should set dash card sixs property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard4').dashData.text).toBe(0));
 
-  it('should create dash card five', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard5')).toBeTruthy());
-  it('should set dash card fives title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard5').dashData.title).toBe('ONLINE STATUS'));
-  it('should set dash card fives property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard5').dashData.text).toBe(''));
+  it('should create dash card nine', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard5')).toBeTruthy());
+  it('should set dash card nines title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard5').dashData.title).toBe('EST ROUNDS TILL NEXT FINDING'));
+  it('should set dash card nines property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard5').dashData.text).toBe(0));
 
-  it('should create dash card six', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard6')).toBeTruthy());
-  it('should set dash card sixs title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard6').dashData.title).toBe('BLOCK VERIFIER ONLINE PERCENTAGE'));
-  it('should set dash card sixs property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard6').dashData.text).toBe(0));
+  it('should create dash card ten', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard6')).toBeTruthy());
+  it('should set dash card tens title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard6').dashData.title).toBe('HOURS SINCE LAST FINDING'));
+  it('should set dash card tens property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard6').dashData.text).toBe(0));
 
-  it('should create dash card seven', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard7')).toBeTruthy());
-  it('should set dash card sevens title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard7').dashData.title).toBe('BLOCK PRODUCER TOTAL ROUNDS'));
-  it('should set dash card sevens property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard7').dashData.text).toBe(0));
+  it('should create dash card four', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard7')).toBeTruthy());
+  it('should set dash card fours title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard7').dashData.title).toBe('VERIFIERS ONLINE ROUNDS'));
+  it('should set dash card fours property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard7').dashData.text).toBe(0));
 
-  it('should create dash card eight', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard8')).toBeTruthy());
-  it('should set dash card eights title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard8').dashData.title).toBe('TOTAL BLOCKS'));
-  it('should set dash card eights property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard8').dashData.text).toBe(0));
+  it('should create dash card three', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard8')).toBeTruthy());
+  it('should set dash card threes title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard8').dashData.title).toBe('VERIFIER ROUNDS'));
+  it('should set dash card threes property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard8').dashData.text).toBe(0));
 
-  it('should create dash card nine', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard9')).toBeTruthy());
-  it('should set dash card nines title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard9').dashData.title).toBe('AVERAGE'));
-  it('should set dash card nines property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard9').dashData.text).toBe(0));
+  it('should create dash card seven', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard9')).toBeTruthy());
+  it('should set dash card sevens title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard9').dashData.title).toBe('PRODUCER ROUNDS'));
+  it('should set dash card sevens property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard9').dashData.text).toBe(0));
 
-  it('should create dash card ten', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard10')).toBeTruthy());
-  it('should set dash card tens title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard10').dashData.title).toBe('ESTIMATED TIME SINCE LAST BLOCK FOUND (IN HOURS)'));
-  it('should set dash card tens property to a string', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard10').dashData.text).toBe(0));
+  it('should create dash card one', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard10')).toBeTruthy());
+  it('should set dash card ones title', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard10').dashData.title).toBe('VOTE COUNT'));
+  it('should set dash card ones property to a number', () => expect(fixture.debugElement.nativeElement.querySelector('#dashcard10').dashData.text).toBe(0));
 
   it('should create delegates table', () => expect(fixture.debugElement.nativeElement.querySelector('#blocks_table')).toBeTruthy());
 
--- src/app/tables/delegates/delegates.component.ts
+++ src/app/tables/delegates/delegates.component.ts
@@ -10,95 +10,101 @@ import { Observable } from 'rxjs';
   templateUrl: './delegates.component.html',
   styleUrls: ['./delegates.component.scss']
 })
+
 export class delegatesComponent implements OnInit {
-        public dashCard1 = [
-        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'TOTAL VOTE COUNT', icon: 'done_all' },
-        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'CURRENT DELEGATE RANK', icon: 'leaderboard' }
+
+  public dashCard1 = [
+        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text_settings: 20, text: 'false', settings: false, title: 'ONLINE STATUS', icon: 'online_prediction' },
+        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'DELEGATE RANK ', icon: 'leaderboard' },
+        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'BLOCKS FOUND ', icon: 'find_in_page' }
     ];
-	public dashCard2 = [
-        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'BLOCK VERIFIER TOTAL ROUNDS', icon: 'autorenew' },
-        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'BLOCK VERIFIERS ONLINE TOTAL ROUNDS', icon: 'sync' }
+  public dashCard2 = [
+        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'ONLINE PERCENTAGE', icon: 'update' },
+        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'EST ROUNDS TILL NEXT FINDING ', icon: 'published_with_changes' },
+        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'HOURS SINCE LAST FINDING', icon: 'alarm_on' }
     ];
-        public dashCard3 = [
-        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text_settings: 20, text: '', settings: false, title: 'ONLINE STATUS', icon: 'online_prediction' },
-        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'BLOCK VERIFIER ONLINE PERCENTAGE', icon: 'update' }
+	public  dashCard3 = [
+        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'VERIFIERS ONLINE ROUNDS', icon: 'model_training' },
+        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'VERIFIER ROUNDS', icon: 'autorenew' },
+        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'PRODUCER ROUNDS', icon: 'find_replace' }
     ];
-	public dashCard4 = [
-        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'BLOCK PRODUCER TOTAL ROUNDS', icon: 'find_replace' },
-        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text_settings: 20, text: 0, settings: true, title: 'TOTAL BLOCKS', icon: 'find_in_page' }
+  public dashCard4 = [
+        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'VOTE COUNT', icon: 'done_all' },
     ];
-   	public dashCard5 = [
-        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text: 0, settings: true, title: 'AVERAGE', icon: 'vertical_align_center' },
-        { colorDark: '#fa741c', colorLight: '#fb934e', width: 40, text_settings: 20, text: 0, settings: true, title: 'ESTIMATED TIME SINCE LAST BLOCK FOUND (IN HOURS)', icon: 'alarm_on' }
-    ];
+
 	public displayedColumns = ['ID', 'Block_Height'];
 	public exampleDatabase = new ExampleDatabase();
 	public dataSource: ExampleDataSource | null;
-  	public showFilterTableCode;
-        delegates_data:string = "";
-        last_block_found:number;
-        title:string;
-  	constructor(private httpdataservice: httpdataservice, private public_address: public_address) { }
+	public showFilterTableCode;
+  delegates_data:string = "";
+  last_block_found:number;
+  title:string;
 
-get_delegates_statistics()
-{
-  this.httpdataservice.get_request(this.httpdataservice.SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_STATISTICS + "?parameter1=" + this.public_address.PUBLIC_ADDRESS).subscribe(
-  (res) =>
+	constructor(private httpdataservice: httpdataservice, private public_address: public_address) { }
+
+  get_delegates_statistics()
   {
-    this.exampleDatabase = new ExampleDatabase();
-    var data = JSON.parse(JSON.stringify(res));
-    this.title = `${data.delegate_name}`;
-    var block_producer_block_heights = data.block_producer_block_heights.split("|");
-    var count;
-    for (count = 0; count < block_producer_block_heights.length; count++)
+    this.httpdataservice.get_request(this.httpdataservice.SERVER_HOSTNAME_AND_PORT_GET_DELEGATES_STATISTICS + "?parameter1=" + this.public_address.PUBLIC_ADDRESS).subscribe(
+    (res) =>
     {
-      if (count == 0)
+      this.exampleDatabase = new ExampleDatabase();
+      var data = JSON.parse(JSON.stringify(res));
+      this.title = `${data.delegate_name}`;
+      var block_producer_block_heights = data.block_producer_block_heights.split("|");
+      var count;
+      for (count = 0; count < block_producer_block_heights.length; count++)
       {
-        continue;
+        if (count == 0)
+        {
+          continue;
+        }
+        this.exampleDatabase.addUser((count).toString(),block_producer_block_heights[count].toString());
+        if (count + 1 == block_producer_block_heights.length)
+        {
+          this.last_block_found = parseInt(block_producer_block_heights[count]);
+        }
       }
-      this.exampleDatabase.addUser((count).toString(),block_producer_block_heights[count].toString());
-      if (count + 1 == block_producer_block_heights.length)
-      {
-        this.last_block_found = parseInt(block_producer_block_heights[count]);
-      }
-    }
-    this.dashCard1[0].text = parseInt(data.total_vote_count) / this.httpdataservice.XCASH_WALLET_DECIMAL_PLACES_AMOUNT;
-    this.dashCard1[1].text = parseInt(data.current_delegate_rank);
-    this.dashCard2[0].text = parseInt(data.block_verifier_total_rounds);
-    this.dashCard2[1].text = parseInt(data.block_verifier_online_total_rounds);
-    this.dashCard3[0].text = data.online_status;
-    this.dashCard3[1].text = parseInt(data.block_verifier_online_percentage);
-    this.dashCard4[0].text = parseInt(data.block_producer_total_rounds);
-    this.dashCard4[1].text = block_producer_block_heights.length-1;
-    this.dashCard5[0].text = parseInt(data.block_verifier_total_rounds) / block_producer_block_heights.length;
-    this.dataSource = new ExampleDataSource(this.exampleDatabase);
-    this.dataSource = new ExampleDataSource(this.exampleDatabase);
-  },
-  (error) =>
-  {
-    Swal.fire("Error","An error has occured","error");
-});
-}
+      this.dashCard1[0].text = (data.online_status).toUpperCase();
+      this.dashCard1[1].text = parseInt(data.current_delegate_rank);
+      this.dashCard1[2].text = block_producer_block_heights.length-1;
 
-get_delegates_website_statistics()
-{
-  this.httpdataservice.get_request(this.httpdataservice.SERVER_HOSTNAME_AND_PORT_GET_DELEGATE_WEBSITE_STATISTICS).subscribe(
-  (res) =>
-  {
-    var data = JSON.parse(JSON.stringify(res));
-    this.dashCard5[1].text = ((parseInt(data.current_block_height) - this.last_block_found) * this.httpdataservice.BLOCK_TIME) / 60;
-  },
-  (error) =>
-  {
-    Swal.fire("Error","An error has occured","error");
+      this.dashCard2[0].text = parseInt(data.block_verifier_online_percentage);
+      this.dashCard2[1].text = parseInt(data.block_verifier_total_rounds) / block_producer_block_heights.length;
+
+      this.dashCard3[0].text = parseInt(data.block_verifier_online_total_rounds);
+      this.dashCard3[1].text = parseInt(data.block_verifier_total_rounds);
+      this.dashCard3[2].text = parseInt(data.block_producer_total_rounds);
+
+      this.dashCard4[0].text = parseInt(data.total_vote_count) / this.httpdataservice.XCASH_WALLET_DECIMAL_PLACES_AMOUNT;
+
+      this.dataSource = new ExampleDataSource(this.exampleDatabase);
+      this.dataSource = new ExampleDataSource(this.exampleDatabase);
+    },
+    (error) =>
+    {
+      Swal.fire("Error","An error has occured","error");
   });
-}
+  }
 
-ngOnInit()
-{
-  this.get_delegates_statistics();
-  this.get_delegates_website_statistics();
-}
+  get_delegates_website_statistics()
+  {
+    this.httpdataservice.get_request(this.httpdataservice.SERVER_HOSTNAME_AND_PORT_GET_DELEGATE_WEBSITE_STATISTICS).subscribe(
+    (res) =>
+    {
+      var data = JSON.parse(JSON.stringify(res));
+      this.dashCard2[2].text = ((parseInt(data.current_block_height) - this.last_block_found) * this.httpdataservice.BLOCK_TIME) / 60;
+    },
+    (error) =>
+    {
+      Swal.fire("Error","An error has occured","error");
+    });
+  }
+
+  ngOnInit()
+  {
+    this.get_delegates_statistics();
+    this.get_delegates_website_statistics();
+  }
 
 
 }

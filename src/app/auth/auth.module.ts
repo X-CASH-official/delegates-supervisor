0efb75f9930d87a0088ab2d311c5712d9fec355b
xcash-rocks <dev@xcash.rocks>
2020-12-27 01:41:06 +0100

    fix: remove import and declaration of AuthComponent

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthComponent } from './auth.component';
 import { RouterModule } from '@angular/router';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -34,7 +33,6 @@ import { CoreModule } from '../core/core.module';
         MatSidenavModule,
         PerfectScrollbarModule,
     ],
-    declarations: [AuthComponent],
     providers: [
         {
             provide: PERFECT_SCROLLBAR_CONFIG,
